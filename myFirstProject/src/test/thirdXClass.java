59afb624c9141f0ea7c139da39e431bc24acbf84
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2017-11-06 23:28:19 +0100

    created a new class

---
+++ myFirstProject/src/test/thirdXClass.java
@@ -0,0 +1,14 @@
+package test;
+
+public class thirdXClass {
+
+	public thirdXClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Third class");
+	}
+
+}
